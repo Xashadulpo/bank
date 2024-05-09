22a1db911ff06f8a1abacac999fa70117666100f
xashadulpo <xashadulasif@gmail.com>
2024-05-09 12:59:09 +0600

    banking

---
+++ src/components/HII.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+const HII = () => {
+  return (
+    <div>
+      <Image src="next.svg" alt="next" width={1000} height={800}/>
+    </div>
+  )
+}
+
+export default HII
